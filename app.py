75fb95d77175001034cfe43adf66be4b7df9e136
azazjhony1 <azaz_jhony@hotmail.com>
2025-12-14 04:12:26 -0600

    change

--- app.py
+++ app.py
@@ -19,6 +19,41 @@ st.set_page_config(
 # st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 # Custom CSS for dark theme
+
+hide_streamlit_style = """
+                <style>
+                div[data-testid="stToolbar"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stDecoration"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stStatusWidget"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                #MainMenu {
+                visibility: hidden;
+                height: 0%;
+                }
+                header {
+                visibility: hidden;
+                height: 0%;
+                }
+                footer {
+                visibility: hidden;
+                height: 0%;
+                }
+                </style>
+                """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+
 st.markdown("""
 <style>
     /* Main background */
